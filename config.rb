3eb68bc877f848fa9f12839139bd336314878f9e
Mark Durrant <mark@pebblecode.com>
2013-02-03 21:13:13 +0000

    add index & edit config.rb

--- config.rb
+++ config.rb
@@ -2,13 +2,13 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
